cc7f6032e337c9d2ede6f685a0c34f4b5285d525
Srinath <47494475+rsrinath14@users.noreply.github.com>
2026-02-01 10:59:44 -0800

    Update train.sh

--- gaussian-splatting/train.sh
+++ gaussian-splatting/train.sh
@@ -1,1 +1,1 @@
-OAR_JOB_ID=radsplat CUDA_VISIBLE_DEVICES=0 python train.py -s data --optimizer_type sparse_adam -d depths -r 1 --eval --antialiasing --iterations 30000 --data_device cpu;
+OAR_JOB_ID=radsplat CUDA_VISIBLE_DEVICES=0 python train.py -s data --optimizer_type sparse_adam -d depths -r 1 --antialiasing --iterations 30000 --data_device cpu;
